62fe08903a5f77f7549e6bc1857a05176d4ec1ca
habyarimanacassien <c.habyarimana@ur.ac.rw>
2024-02-14 12:47:31 +0200

    js file updated

--- project/js/script.js
+++ project/js/script.js
@@ -3,6 +3,10 @@ const search = document.getElementById("search");
 const search_result = document.getElementById("search_result");
 const id_nbr = document.getElementById("id_nbr");
 const names = document.getElementById("names");
+
+//const startuplab = document.getElementById("startuplab");
+//const startupval = document.getElementById("startupval");
+
 const startup = document.getElementById("startup");
 const nov2022 = document.getElementById("nov2022");
 const dec2022 = document.getElementById("dec2022");
@@ -37,23 +41,42 @@ function fetchData() {
             personalData  = data.find(hasid);
             id_nbr.innerHTML = personalData.id;
             names.innerHTML = personalData.names;
-            startup.innerHTML = personalData.tatol_saving_begin_nov_2022;
-            nov2022.innerHTML = personalData.saving_nov_2022;
-            dec2022.innerHTML = personalData.saving_dec_2022;
-            jan2023.innerHTML = personalData.saving_jan_2023;
-            feb2023.innerHTML = personalData.saving_feb_2023;
-            mar2023.innerHTML = personalData.saving_mar_2023;
-            apr2023.innerHTML = personalData.saving_apr_2023;
-            may2023.innerHTML = personalData.saving_may_2023;
-            jun2023.innerHTML = personalData.saving_jun_2023;
-            jul2023.innerHTML = personalData.saving_jul_2023;
-            aug2023.innerHTML = personalData.saving_aug_2023;
-            sep2023.innerHTML = personalData.saving_sep_2023;
-            oct2023.innerHTML = personalData.saving_oct_2023;
-            total.innerHTML = personalData.total;
-            withdraw.innerHTML = personalData.withdrawn;
-            balance.innerHTML = personalData.balance_end_oct_2023;
-            interest.innerHTML = personalData.interest_end_year;
+
+            document.getElementById("startuplab").innerHTML = "StartUp Saving:";
+            document.getElementById('startupval').value = personalData.tatol_saving_begin_nov_2022;
+            document.getElementById("nov2022").innerHTML = "Saving Nov-2022:";
+            document.getElementById('nov2022val').value = personalData.saving_nov_2022;
+
+            document.getElementById('dec2022').innerHTML = "Saving Dec-2022:";
+            document.getElementById('dec2022val').value = personalData.saving_dec_2022;
+            document.getElementById('jan2023').innerHTML = "Saving Jan-2023:";
+            document.getElementById('jan2023val').value = personalData.saving_jan_2023;
+            document.getElementById('feb2023').innerHTML = "Saving Feb-2023:";
+            document.getElementById('feb2023val').value = personalData.saving_feb_2023;
+            document.getElementById('mar2023').innerHTML = "Saving Mar-2023:";
+            document.getElementById('mar2023val').value = personalData.saving_mar_2023;
+            document.getElementById('apr2023').innerHTML = "Saving Apr-2023:";
+            document.getElementById('apr2023val').value = personalData.saving_apr_2023;
+            document.getElementById('may2023').innerHTML = "Saving May-2023:";
+            document.getElementById('may2023val').value = personalData.saving_may_2023;
+            document.getElementById('jun2023').innerHTML = "Saving Jun-2023:";
+            document.getElementById('jun2023val').value = personalData.saving_jun_2023;
+            document.getElementById('jul2023').innerHTML = "Saving Jul-2023:";
+            document.getElementById('jul2023val').value = personalData.saving_jul_2023;
+            document.getElementById('aug2023').innerHTML = "Saving Aug-2023:";
+            document.getElementById('aug2023val').value = personalData.saving_aug_2023;
+            document.getElementById('sep2023').innerHTML = "Saving Sep-2023:";
+            document.getElementById('sep2023val').value = personalData.saving_sep_2023;
+            document.getElementById('oct2023').innerHTML = "Saving Oct-2023:";
+            document.getElementById('oct2023val').value = personalData.saving_oct_2023;
+            document.getElementById('total').innerHTML = "Total saving:";
+            document.getElementById('totalval').value = personalData.total;
+            document.getElementById('withdraw').innerHTML = "Withdrawn amount:";
+            document.getElementById('withdrawval').value = personalData.withdrawn;
+            document.getElementById('balance').innerHTML = "Balance:";
+            document.getElementById('balanceval').value = personalData.balance_end_oct_2023;
+            document.getElementById('interest').innerHTML = "Interest received:";
+            document.getElementById('interestval').value = personalData.interest_end_year;
         
     })
     .catch(() => {
